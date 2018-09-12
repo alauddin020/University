903b2cbcb825ea1e3dc4382d0783be5d3ef2f47a
Alauddin F-a <alauddin020@gmail.com>
2018-09-13 01:42:20 +0600

    Add files via upload

---
+++ FInal/Controllers/SaveCourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FInal.BIL;
+using FInal.Gateway;
+using FInal.Models;
+
+namespace FInal.Controllers
+{
+    public class SaveCourseController : Controller
+    {
+      DepartmentManage aManage = new DepartmentManage();
+        //
+        // GET: /SaveCourse/
+      [HttpGet]
+        public ActionResult Index()
+        {
+           List<Department> departments= aManage.ViewAllDepartment();
+           return View(departments);
+        }
+      [HttpPost]
+      public ActionResult Index(SaveCourse data)
+      {
+        ViewBag.Message = aManage.SaveCourse(data);
+        return View();
+      }
+	}
+}
